8e5fdad90ad4ac263df377265543e9f31d02752b
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-04-05 18:45:33 +0800

    Feat: AppFormInput test

--- src/components/app/__test__/AppFormInput.spec.ts
+++ src/components/app/__test__/AppFormInput.spec.ts
@@ -1,23 +1,12 @@
 import { validationTest } from "@/components/__tests__/helpers/validation.spec";
 import { describe } from "vitest";
-import { AppFormInput } from '../';
 import { namedSlotTest } from '../../__tests__/helpers/slot.spec';
+import { AppFormInput } from '../';
 const name = 'AppFormInput';
 
 
 describe(name, () => {
-  // Slot Tests
-  validationTest(AppFormInput, name);
-  namedSlotTest(AppFormInput, 'prepend', name);
-  namedSlotTest(AppFormInput, 'append', name);
-
-  /** test if hover is working */
-
-  /** test if active is working */
-
-  /** test if disabled css is working */
-
-  /** test if loading css is working */
-  
-  /** test if sizes is working */
+  validationTest(AppFormInput);
+  namedSlotTest(AppFormInput, 'prepend');
+  namedSlotTest(AppFormInput, 'append');
 })
