b651f59eb7371f620554ca5fa370a61aea964909
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-04-05 18:50:35 +0800

    Feat: Add page transition

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import LayoutHandler from './layout/LayoutHandler.vue';
-
+import { TransitionFade } from '@/components/transitions'
 </script>
 
 <template>
   <LayoutHandler>
-    <RouterView></RouterView>    
+    <RouterView  v-slot="{ Component }" >
+      <TransitionFade>
+        <component :is="Component"></component>
+      </TransitionFade>
+    </RouterView>      
   </LayoutHandler>
 </template>
