7a913fc23aafa9baaea943baccfb1bd1f82bc354
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-04-19 22:45:48 +0800

    Style: ComponentCard vertical spacing

--- src/components/ComponentCard.vue
+++ src/components/ComponentCard.vue
@@ -5,39 +5,40 @@ const props = defineProps({
 </script>
 
 <template>
-  <div 
-    class="
-      flex 
-      flex-col 
-      my-5
-      border dark:border-secondary-600
-      rounded
-      max-w-screen 
-    "
-  >
-    <h4 
-      class="
-        p-2
-        rounded-t
-        font-semibold 
-        bg-primary-200 dark:bg-primary-700
-        text-primary-900 dark:text-primary-100
-        truncate
-        capitalize
-      "
-    >
-      {{ props.title }}
-    </h4>
+  <div class="py-5">
     <div 
       class="
-        py-6 p-1 md:p-3 
-        flex-grow
-        rounded-b
-        w-full
-        bg-white dark:bg-secondary-800
+        flex 
+        flex-col 
+        border dark:border-secondary-600
+        rounded
+        max-w-screen 
       "
     >
-      <slot></slot>
+      <h4 
+        class="
+          p-2
+          rounded-t
+          font-semibold 
+          bg-primary-200 dark:bg-primary-700
+          text-primary-900 dark:text-primary-100
+          truncate
+          capitalize
+        "
+      >
+        {{ props.title }}
+      </h4>
+      <div 
+        class="
+          py-6 p-1 md:p-3 
+          flex-grow
+          rounded-b
+          w-full
+          bg-white dark:bg-secondary-800
+        "
+      >
+        <slot></slot>
+      </div>
     </div>
   </div>
 </template>
