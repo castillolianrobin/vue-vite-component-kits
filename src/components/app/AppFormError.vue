65d936760f17631e92730aad2dc719af88296b6d
Lian Robin Castillo <lian@thinkbitsolutions.com>
2023-05-15 18:28:41 +0800

    Feat: Form error transition

--- src/components/app/AppFormError.vue
+++ src/components/app/AppFormError.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
-import type { PropType } from 'vue';
+import { computed, useSlots, type PropType } from 'vue';
 
+/** Slot Composable */
+const slots = useSlots();
 
 const props = defineProps({
   error: String as PropType<string>,
-})
+});
+
+const hasError = computed(()=>
+ props.error || slots.default && slots.default()[0].children
+);
 
 </script>
 
 <template>
-  <p 
-    :class="`text-error-500 text-xs`"
-    aria-label="error"
-    role="alert"
+  <Transition
+    enter-active-class="duration-500 ease-out"
+    enter-from-class="max-h-0 opacity-0"
+    enter-to-class="max-h-12 opacity-100"
+    leave-active-class="duration-200 ease-in"
+    leave-from-class="max-h-12 opacity-100"
+    leave-to-class="max-h-0 opacity-0"
   >
-    <slot>{{ props.error }}</slot>
-  </p>
+    <p
+      v-if="hasError" 
+      :class="`text-error-500 text-xs`"
+      aria-label="error"
+      role="alert"
+    >
+      <slot>{{ props.error }}</slot>
+    </p>
+  </Transition>
 </template>
